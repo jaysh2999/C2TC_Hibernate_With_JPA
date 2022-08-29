0b6b09d7c75737fab1ce34901011b6f4cc87534f
jaysh2999 <92245778+jaysh2999@users.noreply.github.com>
2022-08-29 20:24:51 +0530

    Add files via upload

--- JPAUtil.java
+++ JPAUtil.java
@@ -1,25 +1,23 @@
-package com.tns.dao;
+package com.cg.jpacrud.dao;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class JPAUtil 
-{
-	 private static EntityManagerFactory factory;
-	 
-	 private static EntityManager entityManager;
-	 
-	 static
-	 {
-		
-		factory = Persistence.createEntityManagerFactory("JPA-PU");
-	 }
-	 public static EntityManager getEntityManager()
-	 {
-		 if(entityManager==null || !entityManager.isOpen()) {
-			 entityManager = factory.createEntityManager();
-		 }
-		 return entityManager;
-	 }
+public class JPAUtil {
 
+	private static EntityManagerFactory factory;
+	private static EntityManager entityManager;
+	
+	static {
+		factory = Persistence.createEntityManagerFactory("JPA-PU");
+	}
+	
+	public static EntityManager getEntityManager() {
+		if(entityManager==null || !entityManager.isOpen()) {
+			entityManager = factory.createEntityManager();
+		}
+		return entityManager;
+	}
+	
 }
